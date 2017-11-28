cb38f6f64a483e7d04f84d335681c78de61ff1d7
Michael Lanzetta <milanz@microsoft.com>
2017-11-28 14:57:18 +0900

    In train_test_split, skipping directory with too few images.

--- scripts/train_test_split.py
+++ scripts/train_test_split.py
@@ -76,7 +76,8 @@ def create_image_lists(image_dir, testing_percentage, validation_percentage):
             logger.warning('No files found')
             continue
         if len(file_list) < 20:
-            logger.warning('WARNING: Folder has less than 20 images, which may cause issues.')
+            logger.warning('WARNING: Folder has less than 20 images, which may cause issues. Skipping.')
+            continue
         elif len(file_list) > MAX_NUM_IMAGES_PER_CLASS:
             logger.warning(
                 'WARNING: Folder {} has more than {} images. Some images will never be selected. Skipping.'
